b6e72f6ac435b0ce319829fa11512935565fd43e
seemefly1982 <seemefly1982@163.com>
2013-08-27 09:47:38 +0800

    Initial Commit

---
+++ WPaper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WPaper
+//
+//  Created by Lee on 13-8-27.
+//  Copyright (c) 2013年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WPAppDelegate class]));
+    }
+}
